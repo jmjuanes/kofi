137dcd69ebe805683955c4c7b585ad21a068b24c
Josemi <josemijuanes@gmail.com>
2019-09-26 16:50:52 +0200

    Removed unused

--- src/dom/index.js
+++
@@ -1,5 +0,0 @@
-export {createNode, removeNode} from "./node.js";
-export {addNodeChildren, removeNodeChildren} from "./children.js";
-export {createRef} from "./ref.js";
-export {Fragment} from "./fragment.js";
-
