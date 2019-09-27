fa76dc4bc3ac88abda9626598096fe66f1208d26
Josemi <josemijuanes@gmail.com>
2019-09-27 10:36:42 +0200

    Big rewrite: export default kofi namespace and fixed import paths

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-export * from "./dispatch.js";
-//export * from "./md.js";
-export * from "./queue.js";
-export * from "./ready.js";
-export * from "./dom/index.js";
-export * from "./helpers/index.js";
-export * from "./request/request.js";
-export * from "./request/url.js";
-export * from "./router/index.js";
+import {addNodeChildren, removeNodeChildren} from "./dom/children.js";
+import {createNode, removeNode} from "./dom/node.js";
+import {Fragment} from "./dom/fragment.js";
+import {createRef} from "./dom/ref.js";
+import {qs} from "./query-string.js";
+import {url} from "./url.js";
+import {request, HTTPError} from "./request.js";
+import {dispatch} from "./dispatch.js";
+import {queue} from "./queue.js";
+import {ready} from "./ready.js";
+import {router} from "./router.js";
+import * as helpers from "./helpers/index.js";
+
+//Build kofi object
+const kofi = {
+    createNode,
+    removeNode,
+    addNodeChildren,
+    removeNodeChildren,
+    Fragment,
+    createRef,
+    request,
+    HTTPError,
+    url,
+    qs,
+    router,
+    dispatch,
+    queue,
+    ready,
+    "helpers"
+};
+
+//Export kofi object
+export default kofi;
 
