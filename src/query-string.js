8c63276a3f8579e150ac7d8dce1b8189be2504c4
Josemi <josemijuanes@gmail.com>
2019-11-21 09:28:58 +0100

    Improve query-string parser and builder. Renamed exported methods

--- src/query-string.js
+++ src/query-string.js
@@ -4,60 +4,50 @@ let unescape = function (str) {
 };
 
 //Convert a query object to string
-let stringify = function (obj, del) {
-    if (typeof obj !== "object") {
-        return obj;
+export function buildQueryString (obj, delimiter) {
+    if (typeof obj !== "object" || obj === null) {
+        return ""; //No object provided --> return an empty string
     }
-    if (typeof del !== "string") {
-        //Default delimiter
-        del = "&";
+    if (typeof delimiter !== "string") {
+        delimiter = "&"; //Default delimiter
     }
-    let str = [];
+    let items = [];
     Object.keys(obj).forEach(function (key) {
-        //Insert into the array
-        str.push(encodeURIComponent(key) + "=" + encodeURIComponent(obj[key]));
+        let items = (Array.isArray(obj[key]) === true) ? obj[key] : [obj[key]];
+        items.forEach(function (value) {
+            items.push(encodeURIComponent(key) + "=" + encodeURIComponent(value));
+        });
     });
-    return str.join(del);
-};
+    return items.join(delimiter);
+}
 
 //Parse a query string
-let parse = function (str, del) {
-    //Check for no string provided
+export function parseQueryString (str, delimiter) {
     if (typeof str !== "string") {
-        return {};
+        return {}; //No querystring provided --> return empty object
     }
-    //Default delimiter
-    if (typeof del !== "string") {
-        del = "&";
+    if (typeof delimiter !== "string") {
+        delimiter = "&";
     }
     let obj = {};
-    str.trim().replace(/^\?/, "").split(del).forEach(function (partial) {
-        if (partial.trim() === "") {
-            return;
-        }
-        let items = partial.trim().split("=");
-        let itemKey = unescape(items[0]);
-        let itemValue = (typeof items[1] === "string") ? unescape(items[1]) : "";
-
-        //Check if value already exists in the parsed object
-        if (typeof obj[itemKey] !== "undefined") {
-            if (Array.isArray(obj[itemKey]) === false) {
-                //Convert to array the current value
-                obj[itemKey] = [obj[itemKey]];
+    //Split by delimiter
+    str.trim().replace(/^\?/, "").split(delimiter).forEach(function (item) {
+        if (item.trim() !== "") {
+            let items = item.trim().split("=");
+            let itemKey = unescape(items[0]);
+            let itemValue = (typeof items[1] === "string") ? unescape(items[1]) : "";
+            //Check if value exists in the parsed object
+            if (typeof obj[itemKey] !== "undefined") {
+                if (Array.isArray(obj[itemKey]) === false) {
+                    obj[itemKey] = [obj[itemKey]];  //Convert to array
+                }
+                obj[itemKey].push(itemValue); //Insert this value
             }
-            obj[itemKey].push(itemValue);
-        }
-        else {
-            obj[itemKey] = itemValue;
+            else {
+                obj[itemKey] = itemValue;
+            }
         }
     });
     return obj;
-};
-
-//Export all qs methods
-export const qs = {
-    unescape,
-    stringify,
-    parse
-};
+}
 
