d23163d7b138fabe8f071560c9854ce3003435b6
Josemi <josemijuanes@gmail.com>
2020-12-04 19:56:00 +0100

    Removed scripts from old versions

--- scripts/version.js
+++
@@ -1,9 +0,0 @@
-let fs = require("fs");
-let path = require("path");
-
-let pkgPath = path.join(process.cwd(), "package.json");
-//Read the package content
-let pkg = JSON.parse(fs.readFileSync(pkgPath, "utf8"));
-//Print the package version
-process.stdout.write(pkg.version);
-
