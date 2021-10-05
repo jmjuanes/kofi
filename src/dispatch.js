2a68fe36c6420034abd683cba73d50f23de56aa7
Josemi <josemijuanes@gmail.com>
2021-10-05 20:46:44 +0200

    Rewrite event dispatcher

--- src/dispatch.js
+++ src/dispatch.js
@@ -1,53 +1,25 @@
-//Dispatch handler
-export class Dispatcher {
-    constructor() {
-        this._listeners = {};
-    }
-    //Register a new event listener
-    addListener(name, listener) {
-        if (typeof this._listeners[name] === "undefined") {
-            this._listeners[name] = [];
-        }
-        //Register the listener
-        this._listeners[name].push(listener);
-    }
-    //Remove a listener
-    removeListener(name, listener) {
-        if (typeof this._listeners[name] !== "undefined") {
-            //Find the listener 
-            let list = this._listeners[name].filter(function (value) {
-                return value !== listener;
-            });
-            this._listeners[name] = list;
-        }
-    };
-    //Remove all listeners of an event 
-    removeAllListeners(name) {
-        if (typeof this._listeners[name] !== "undefined") {
-            this._listeners[name] = [];
-        }
-    };
-    //Emit an event
-    emit(name) {
-        //Check if there are listeners registered for this event
-        if (typeof this._listeners[name] !== "undefined") {
-            //Generate the arguments to be passed to the listeners
-            let args = [];
-            for (let i = 1; i < arguments.length; i++) {
-                args.push(arguments[i]);
-            }
-            //Call each listener
-            let listeners = this._listeners[name];
-            for (let i = 0; i < listeners.length; i++) {
-                listeners[i].apply(null, args);
-            }
-        }
-        return;
-    }
-}
-
 //Build a new dispatcher
-export function dispatch () {
-    return new Dispatcher();
-}
+export const dispatch = () => {
+    const listeners = {}; //Event listeners
 
+    // Method to register a new event listener
+    const on = (name, listener) => {
+        if (typeof listeners[name] === "undefined") {
+            listeners[name] = [];
+        }
+        listeners[name].push(listener);
+    };
+
+    //Method to remove an event listener
+    const off = (name, listener) => {
+        listeners[name] = (listeners[name] || []).filter(f => f !== listener);
+    };
+
+    // Dispatch event
+    const dispatch = (name, ...args) => {
+        (listeners[name] || []).forEach(f => f(...args));
+    };
+
+    //Return dispatch methods
+    return {on, off, dispatch};
+};
