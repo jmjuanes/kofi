5c38cbc021ea4bbd5ab543a07bca839633ca514a
Josemi <josemijuanes@gmail.com>
2021-12-09 21:03:53 +0100

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import cleanup from "rollup-plugin-cleanup";
 import {version} from "./package.json";
 
 //Generate the banner
-let banner = [];
+const banner = [];
 banner.push("/*!*");
 banner.push(" * @license kofi " + version);
 banner.push(" *");
@@ -12,33 +12,19 @@ banner.push(" * LICENSE file in the root directory of this source tree.");
 banner.push(" */");
 banner.push("");
 
-//Output formats
-let outputFormats = ["umd", "umd.min", "cjs", "cjs.min", "esm", "esm.min"];
-
 //Initialize the configuration object
 export default {
-    "input": "src/index.js",
-    "output": outputFormats.map(function (ext) {
-        let output = {
-            "file": `./dist/kofi.${ext}.js`,
-            "format": ext.replace(".min", ""),
-            "banner": banner.join("\n")
-        };
-        //Check for UMD format --> add export name
-        if (ext.indexOf("umd") !== -1) {
-            output["name"] = "kofi";
-        }
-        //Check for min format
-        if (ext.indexOf(".min") !== -1) {
-            output["plugins"] = [terser({
-                "output": {
-                    "comments": "all"
-                }
-            })];
-        }
-        //Return the output format config
-        return output;
-    }),
-    "plugins": [cleanup()]
+    "input": "kofi.js",
+    "output": ["umd", "esm"].map(format => ({
+        "file": `./kofi.${format}.js`,
+        "format": format, 
+        // "banner": banner.join("\n"),
+        "name": format === "umd" ? "kofi": undefined,
+        "plugins": [
+            terser(),
+        ],
+    })),
+    "plugins": [
+        cleanup(),
+    ],
 };
-
