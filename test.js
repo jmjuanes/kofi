b3b2f8e21195332e750abb5bf04ef9ad361909d7
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-12-17 00:10:47 +0000

    Update tests

--- test.js
+++ test.js
@@ -11,4 +11,37 @@ describe("htm", () => {
         render(parent, htm`<div class="test"></div>`);
         expect(parent.querySelector(`div.test`)).toBeDefined();
     });
+
+    it("should set text attributes", () => {
+        const className = "test";
+        render(parent, htm`<button class="${className}"></button>`);
+        expect(Array.from(parent.querySelector("button").classList)).toContain(className);
+    });
+    
+    it("should set boolean attributes", () => {
+        const isChecked = true;
+        const element = htm`
+            <input type="checkbox" ${isChecked ? "checked" : ""}>
+        `;
+
+        render(parent, element);
+        expect(parent.querySelector("input").checked).toEqual(isChecked);
+    });
+
+    it("should set events", () => {
+        const handleClick1 = jest.fn();
+        const handleClick2 = jest.fn();
+        const template = htm`
+            <div>
+                <div id="test1" onClick="${handleClick1}"></div>
+                <div id="test2" onClick="${handleClick2}"></div>
+            </div>
+        `;
+
+        render(parent, template);
+        // parent.querySelector(`div#test1`).click();
+        parent.querySelector(`div#test2`).click();
+        expect(handleClick1).not.toHaveBeenCalled();
+        expect(handleClick2).toHaveBeenCalled();
+    });
 });
