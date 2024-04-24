77e0a19dbaca262cdb42af2e55ccbd981413e3a6
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-04-24 21:43:30 +0200

    feat: add html function to convert template literal to vdom (#6)

--- kofi.js
+++ kofi.js
@@ -1,6 +1,15 @@
 // Self closing tags
 const selfClosingTags = new Set(["link", "meta", "input", "br", "img", "hr"]);
 
+// Modes for parsing template literal to VDOM
+const HTML_TEMPLATE_MODE = {
+    TEXT: 1,
+    TAG_START: 2,
+    TAG_END: 3,
+    PROP_NAME: 4,
+    PROP_VALUE: 5,
+};
+
 // Set a property
 const setProperty = (el, name, value = null) => {
     // Check for style property and string value --> set as css string
@@ -19,7 +28,6 @@ const setProperty = (el, name, value = null) => {
     }
     // Default: set the property value
     else if (name !== "html") {
-        // el.setAttribute(key, value)
         el[name] = value;
     }
 };
@@ -46,9 +54,9 @@ const nodesDiffs = (node1, node2) => {
 const kofi = (type, props, ...children) => {
     if (type && typeof type === "string") {
         return {
-            "type": type.toLowerCase().trim(), 
-            "props": props || {}, 
-            "children": (children || []).flat().filter(c => !!c),
+            type: type.toLowerCase().trim(), 
+            props: props || {}, 
+            children: (children || []).flat().filter(c => !!c),
         };
     }
     // Check for function type
@@ -57,6 +65,148 @@ const kofi = (type, props, ...children) => {
     }
     // Other type --> throw error
     throw new Error("Invalid element type provided");
+};
+
+// @private convert a template literal to a VDOM
+kofi.template = (h, literal, values, ctx = {i: 0, j: 0}, closing = null) => {
+    const children = [];
+    let mode = HTML_TEMPLATE_MODE.TEXT, buffer = "", current = null, quote = null;
+    while (ctx.i < literal.length) {
+        while (ctx.j < literal[ctx.i].length) {
+            const char = literal[ctx.i][ctx.j];
+            const nextChar = literal[ctx.i][ctx.j + 1];
+            // We found a '<' character
+            if (mode === HTML_TEMPLATE_MODE.TEXT && char === "<") {
+                if (buffer.trim() !== "") {
+                    children.push(buffer);
+                }
+                buffer = "";
+                mode = HTML_TEMPLATE_MODE.TAG_START;
+            }
+            // We are in TAG_START mode and we found a '/' character with empty buffer
+            else if (mode === HTML_TEMPLATE_MODE.TAG_START && char === "/" && !buffer) {
+                mode = HTML_TEMPLATE_MODE.TAG_END;
+            }
+            // We are in TAG_START mode and we found a '/' character with non empty buffer
+            else if (mode === HTML_TEMPLATE_MODE.TAG_START && char === "/" && buffer && nextChar === ">") {
+                children.push(h(buffer, {}));
+                buffer = "";
+                mode = HTML_TEMPLATE_MODE.TEXT;
+                ctx.j = ctx.j + 1; // Skip next '>'
+            }
+            // We are in TAG_START mode and we found a '>' character
+            else if (mode === HTML_TEMPLATE_MODE.TAG_START && char === ">") {
+                ctx.j = ctx.j + 1;
+                children.push(h(buffer, {}, ...(kofi.template(h, literal, values, ctx, buffer) || [])));
+                buffer = "";
+                mode = HTML_TEMPLATE_MODE.TEXT;
+            }
+            // We are in TAG_START mode and we found a whitespace character
+            else if (mode === HTML_TEMPLATE_MODE.TAG_START && char === " ") {
+                current = [buffer, {}, [], ""];
+                buffer = "";
+                mode = HTML_TEMPLATE_MODE.PROP_NAME;
+            }
+            // We are in PROP_NAME mode and we found a '/' character
+            else if (mode === HTML_TEMPLATE_MODE.PROP_NAME && char === "/" && nextChar === ">") {
+                if (buffer.trim() !== "") {
+                    current[1][buffer.trim()] = true;
+                }
+                children.push(h(current[0], current[1], ...current[2]));
+                current = null;
+                mode = HTML_TEMPLATE_MODE.TEXT;
+                buffer = "";
+                ctx.j = ctx.j + 1; // Skip next '>'
+            }
+            // We found a '>' character and we are in property name mode
+            else if (mode === HTML_TEMPLATE_MODE.PROP_NAME && char === ">") {
+                if (buffer.trim() !== "") {
+                    current[1][buffer.trim()] = true;
+                }
+                ctx.j = ctx.j + 1;
+                current[2] = kofi.template(h, literal, values, ctx, current[0]);
+                children.push(h(current[0], current[1], ...current[2]));
+                current = null;
+                mode = HTML_TEMPLATE_MODE.TEXT;
+                buffer = "";
+            }
+            // We found a '>' character and we are in closing tagname
+            else if (mode === HTML_TEMPLATE_MODE.TAG_END && char === ">") {
+                if (buffer !== closing) {
+                    throw new Error(`Unexpected closing tag. Expected '${closing}' but got '${buffer}'`);
+                }
+                return children;
+            }
+            // We found a '=' character and we are in PROP_NAME mode
+            else if (mode === HTML_TEMPLATE_MODE.PROP_NAME && char === "=") {
+                current[3] = buffer.trim();
+                buffer = "";
+                mode = HTML_TEMPLATE_MODE.PROP_VALUE;
+                if (nextChar === `"` || nextChar === `'`) {
+                    ctx.j = ctx.j + 1;
+                    quote = nextChar;
+                }
+            }
+            // We found a whitespace character and we are in PROP_NAME mode
+            else if (mode === HTML_TEMPLATE_MODE.PROP_NAME && char === " ") {
+                if (buffer.trim() !== "") {
+                    current[1][buffer.trim()] = true;
+                }
+                buffer = "";
+            }
+            // End of property value
+            else if (mode === HTML_TEMPLATE_MODE.PROP_VALUE && char === quote) {
+                current[1][current[3]] = buffer;
+                quote = null;
+                buffer = "";
+                mode = HTML_TEMPLATE_MODE.PROP_NAME;
+            }
+            // Other case, save character in buffer
+            else {
+                buffer = buffer + char;
+            }
+            ctx.j++;
+        }
+        ctx.j = 0;
+        if (ctx.i < values.length) {
+            // Check if we are in PROP_VALUE mode
+            if (mode === HTML_TEMPLATE_MODE.PROP_VALUE) {
+                current[1][current[3]] = values[ctx.i];
+                mode = HTML_TEMPLATE_MODE.PROP_NAME;
+                if (literal[ctx.i + 1][ctx.j] === quote) {
+                    ctx.j = ctx.j + 1;
+                }
+            }
+            // Check if we are in PROP_NAME and current prop is '...'
+            else if (mode === HTML_TEMPLATE_MODE.PROP_NAME && buffer === "...") {
+                Object.assign(current[1], values[ctx.i]);
+                buffer = "";
+            }
+            // Check if we are in TAG_START or TAG_END modes
+            else if (mode === HTML_TEMPLATE_MODE.TAG_START || mode === HTML_TEMPLATE_MODE.TAG_END) {
+                buffer = values[ctx.i];
+            }
+            // Check if we are in text mode
+            else if (mode === HTML_TEMPLATE_MODE.TEXT) {
+                if (buffer.trim() !== "") {
+                    children.push(buffer);
+                }
+                children.push(values[ctx.i]);
+                buffer = "";
+            }
+        }
+        ctx.i++;
+    }
+    // If we are at the end of the literal and we have buffer not added
+    if (buffer.trim() !== "") {
+        children.push(buffer);
+    }
+    return children;
+};
+
+// @public convert a tagged template to a kofi's vdom
+kofi.html = (literal, ...values) => {
+    return kofi.template(kofi, literal, values, {i: 0, j: 0}, null)[0];
 };
 
 // Render an element
