138dcbb83e799ba12dab2258f455b1ec122aa4c6
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2026-01-03 21:15:16 +0100

    Rename kofi.js to index.js and fix imports

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-import {describe, it} from "node:test";
+import { describe, it } from "node:test";
 import assert from "node:assert";
-import kofi from "./kofi.js";
+import kofi from "./index.js";
 
 describe("kofi.template", () => {
     const jsx = (literal, ...values) => {
